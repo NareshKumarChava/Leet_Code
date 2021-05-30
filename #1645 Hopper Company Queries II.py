56e3f01e0fbd79bd4402a29ee06c1092bba81850
nchava <34551187+NareshKumarChava@users.noreply.github.com>
2021-05-29 21:46:01 -0700

    upload sql files

---
+++ #1645 Hopper Company Queries II.py
@@ -0,0 +1,22 @@
+"""
+Author: Naresh Chava
+Date: 05/28/2021
+
+Language: MYSQL 
+
+
+Write an SQL query to report the percentage of working drivers (working_percentage) for each month of 2020 where:
+
+
+
+Note that if the number of available drivers during a month is zero, we consider the working_percentage to be 0.
+
+Return the result table ordered by month in ascending order, where month is the month's number (January is 1, February is 2, etc.). Round working_percentage to the nearest 2 decimal places.
+
+The query result format is in the following example.
+
+
+"""
+
+
+
